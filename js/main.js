3086580290ea4bdc1298fa46546b0e9528ad251a
MeLlamoLeandro <lea_giffoni@hotmail.com>
2023-03-11 23:10:34 -0300

    fx ppal funcionando

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-alert("Bienvenido a Banco La Tenaza!!!\n\nEste simulador le permitira caclular la serie de pagos a realizar\ncuando solicita un prestamos a  tasa fija con sistema de amortización francés.\n");
+alert("Bienvenido a Banco Mónaco!!!\n\nEste simulador le permitira caclular la serie de pagos a realizar\ncuando solicita un prestamos a tasa fija con sistema de amortización francés.\n");
 
 //Declaro constantes y variables GLOBALES del simulador.
 const iva = 0.21;//impuesto valor agregado, se imputa sobre los intereses de cada cuota
@@ -21,43 +21,39 @@ condiciones.push(new Condicion(60, 103));
 condiciones.push(new Condicion(72, 103));
 
 
-//Inicio la variables pidiendo los datos por PROMPT.
-//Solicito las condiciones para la simulación.
-iCondiciones = prompt(`Por favor, seleccione las condiciones del crédito que desea simular:\n
+//Inicio la variables pidiendo los datos por PROMPT y controlo los datos ingresados. Valido si estan dentro de los parametros del simulador.
+
+//Solicito el monto sobre el que calculo el credito.
+let monto = parseInt(prompt("Por favor ingrese el monto a solicitar."));
+while (monto <= 0) {
+    monto = parseInt(prompt("Por favor ingrese un monto mayor a 0 (cero)."));
+}
+
+//Solicito las condiciones de pago
+iCondiciones = parseInt(prompt(`Por favor, seleccione las condiciones del crédito que desea simular:\n
 1 --> Plazo ${condiciones[0].plazo} meses y T.N.A ${condiciones[0].tna}%.\n
 2 --> Plazo ${condiciones[1].plazo} meses y T.N.A ${condiciones[1].tna}%.\n
 3 --> Plazo ${condiciones[2].plazo} meses y T.N.A ${condiciones[2].tna}%.\n
 4 --> Plazo ${condiciones[3].plazo} meses y T.N.A ${condiciones[3].tna}%.\n
 5 --> Plazo ${condiciones[4].plazo} meses y T.N.A ${condiciones[4].tna}%.\n
 6 --> Plazo ${condiciones[5].plazo} meses y T.N.A ${condiciones[5].tna}%.\n
-7 --> Plazo ${condiciones[6].plazo} meses y T.N.A ${condiciones[6].tna}%.`);
+7 --> Plazo ${condiciones[6].plazo} meses y T.N.A ${condiciones[6].tna}%.`));
 
-//Controlo los datos ingresados. Validar si estan dentro de los parametros del simulador.
-
-
-let monto = parseInt(prompt("Por favor ingrese el monto a solicitar."));
-while (monto <= 0) {
-    monto = parseInt(prompt("Por favor ingrese un monto mayor a 0 (cero)."));
-}
-/*
-let plazo = parseInt(prompt("Por favor ingrese el plazo en meses.(12 min.-72 máx.)."));
-while ((plazo < pmin) || (plazo > pmax)){ 
-    plazo = parseInt(prompt("Por favor ingrese el plazo en meses.(12 min.-72 máx.)."));
-}    
-
-let tna = parseFloat(prompt("Por favor ingrese la Tasa Nominal Anual(T.N.A) en números. Ejemplo '96.5' --> 96.5% T.N.A"));
-while (tna <= 0){   
-    tna = parseFloat(prompt("Por favor ingrese una T.N.A mayor a 0 (cero)."));
+while (iCondiciones < 1 || iCondiciones > 7) {
+    iCondiciones = parseInt(prompt(`Por favor seleccione una opcion del 1 al 7`));
 }
 
-console.log('Simulando una serie de pagos para un crédito por: $'+ monto);
-console.log("a devolver en un período de " + plazo + " meses");
-console.log("con una tasa del " + tna + " %.");
-console.log("------------------------------------------");
+let n = iCondiciones - 1;
+
+alert(`Simulando una serie de pagos para un crédito por: $${monto}\n
+a devolver en un período de ${condiciones[n].plazo} meses\n
+con una tasa del ${condiciones[n].tna}%`);
+
+//------------------------------------------------------------------------
 // Creo un Array para almacenar los pagos mensuales
 const pagos = [];
-//------------------------------------------------------------------------
-//Declaro la funcion
+
+//Declaro la funcion principal del simulador.
 const calcularPagos = (monto,plazo,tna)=>{
     //Calculo la tasa mensual - declaro e inicio la constante de tasa mensual
     const tMensual = tna /100 /12;
@@ -69,11 +65,8 @@ const calcularPagos = (monto,plazo,tna)=>{
     let capital;
     let cuotaTotal;
 
-    
-
     // Calculo los pagos mensuales desde el mes 1 al seleccionado con un bucle.
-    for (let i = 1; i <= plazo; i++) {
-        
+    for (let i = 1; i <= plazo; i++) {    
         //segun investigue, para calcular la primera cuota, el Saldo de Deuda es el equivalente al Monto solicitado
         //como en el simulador del banco ICBC y del BNA online.
         if (i === 1) {
@@ -81,7 +74,7 @@ const calcularPagos = (monto,plazo,tna)=>{
         }else{
             saldoDeuda = saldoDeuda - capital;
         }
-        
+
         //Calculo el resto de los componentes de la cuota
         interes = saldoDeuda * tMensual;
         capital = cuotaPura - interes;
@@ -100,7 +93,6 @@ const calcularPagos = (monto,plazo,tna)=>{
         })
         
         // Muestro el resultado de cada cuota en consola redondeando a dos decimales
-        
         console.log("Mes: " + pagos[i-1].mes);
         console.log("Saldo Deuda: $" + pagos[i-1].saldoDeuda);
         console.log("Cuota pura: $" + pagos[i-1].cuotaPura);
@@ -113,7 +105,14 @@ const calcularPagos = (monto,plazo,tna)=>{
 }
 
 //Invoco la funcion calcularPagos
-calcularPagos(monto,plazo,tna);
-console.log(pagos);
+calcularPagos(monto,condiciones[n].plazo,condiciones[n].tna);
+/*
+busqueda = prompt("Desea buscar el detalle de un mes particular? (S/N)");
+if (busqueda.upCase() == 'S') {
+    bMes = parseInt(prompt("Por favor, ingrese el mes que desea buscar."));
+    //funcion para buscar el detalle de un mes
 
+}else{
+    alert("Muchas gracias por usar el simulador. Por favor, vuelva");
+}
 */
