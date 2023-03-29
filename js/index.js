82148c8c562952c4e2283289a52970300cf05e28
MeLlamoLeandro <lea_giffoni@hotmail.com>
2023-03-29 02:15:28 -0300

    simulador terminado

--- js/index.js
+++ js/index.js
@@ -351,10 +351,15 @@ function renderResultados() {
   const info = cargarInfoLS();
   const pagos = cargarSimulacionLS();
   if (info.length != []) {
-    let salida = `<div class="alert alert-success" role="alert">
-    Continua tu simulación desde donde la dejaste!
-    <button type="button" class="btn btn-danger" onclick="borrarTodo()">Borrar Todo</button>
-  </div>`;
+    let salida = `
+    <div class="alert alert-success d-flex align-items-center justify-content-between" role="alert">
+      <div>
+        <p class="fs-2">Continua tu simulación desde donde la dejaste!</p>
+      </div>
+      <div>
+        <button type="button" class="btn btn-danger" onclick="borrarTodo()">Borrar Todo</button>
+      </div>
+    </div>`;
     document.getElementById("continua").innerHTML = salida;
     if (pagos.length > 0) {
       mostrarResultados(pagos);
