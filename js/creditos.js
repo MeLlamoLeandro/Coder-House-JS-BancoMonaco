a067e254fdc208f9546ef5d525b29fbd3a7ea8fe
MeLlamoLeandro <lea_giffoni@hotmail.com>
2023-04-08 20:33:09 -0300

    probando metodo fetch

--- js/creditos.js
+++ js/creditos.js
@@ -1,30 +1,33 @@
-//Armo una funcion constructora para los plazos y tasas.
-//Cuando hay nuevas combinacionesd de cuotas y tasas de interes solo hay que mantener esta parte del codigo porque despues se cargan con la funcion "cargarCondiciones()"
-class Condicion {
-  constructor(plazo, tna) {
-    this.plazo = plazo;
-    this.tna = tna;
-  }
-}
+// cargo las condiciones desde el archivo condiciones.json usando el método Fech
 const condiciones = [];
-condiciones.push(new Condicion(12, 99.5));
-condiciones.push(new Condicion(18, 99.5));
-condiciones.push(new Condicion(24, 99.5));
-condiciones.push(new Condicion(36, 106.5));
-condiciones.push(new Condicion(48, 106.5));
-condiciones.push(new Condicion(60, 106.5));
-condiciones.push(new Condicion(72, 106.5));
-condiciones.push(new Condicion(84, 106.5));
-condiciones.push(new Condicion(96, 106.5));
+fetch("condiciones.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const condiciones = data.condiciones;
+    guardarCondicionesLS(condiciones);
+  })
+  .catch((error) => console.error(error));
 
-//almaceno las diferenctes posibilidades de credito en el Local Storage
+//almaceno las diferentes posibilidades de credito en el Local Storage
 function guardarCondicionesLS(condiciones) {
   localStorage.setItem("condiciones", JSON.stringify(condiciones));
 }
 
-//cargo las condiciones en el SELECT #selectCond del HTML
+//cargo las condiciones desde el LS para cargar en el SELECT #selectCond del HTML
 function cargarCondicionesLS() {
   return (optCond = JSON.parse(localStorage.getItem("condiciones")) || []);
 }
 
-guardarCondicionesLS(condiciones);
+function renderSelect() {
+  const opciones = cargarCondicionesLS();
+  const select = document.getElementById("selectCond");
+  let salida = "";
+  for (let i = 0; i < opciones.length; i++) {
+    const opcion = opciones[i];
+    salida += `<option value="${i}">${opcion.plazo} meses - TNA: ${opcion.tna}%</option>`;
+  }
+  select.innerHTML = salida;
+}
+
+//guardarCondicionesLS(condiciones);
+renderSelect();
--- js/index.js
+++ js/index.js
@@ -332,16 +332,7 @@ const buscarFecha = () => {
 //Renders
 const info = JSON.parse(localStorage.getItem("info")) || [];
 
-function renderSelect() {
-  const opciones = cargarCondicionesLS();
-  const select = document.getElementById("selectCond");
-  let salida = "";
-  for (let i = 0; i < opciones.length; i++) {
-    const opcion = opciones[i];
-    salida += `<option value="${i}">${opcion.plazo} meses - TNA: ${opcion.tna}%</option>`;
-  }
-  select.innerHTML = salida;
-}
+
 
 function cargarInfoLS() {
   return JSON.parse(localStorage.getItem("info")) || [];
