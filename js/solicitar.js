02227bec09ef3c23a3eab56e9a61e24905293e4d
MeLlamoLeandro <lea_giffoni@hotmail.com>
2023-04-15 19:14:45 -0300

    card info ok

--- js/solicitar.js
+++ js/solicitar.js
@@ -1,4 +1,60 @@
 const info = cargarInfoLS();
+let cardInfo = document.getElementById("credSelect");
+
+function credSelect() {
+  const info = cargarInfoLS();
+  if (info == "") {
+    salida = "";
+  } else {
+    salida = `<ul class="list-group mb-3">
+                          <li class="list-group-item d-flex justify-content-between lh-sm">
+                            <div>
+                              <h6 class="my-0">Monto del Préstamo</h6>
+                                <small class="text-body-secondary">Pesos</small>
+                            </div>
+                            <span class="text-body-secondary ">$${separadorMiles(
+                              info.monto
+                            )}</span>
+                          </li>
+                          <li class="list-group-item d-flex justify-content-between lh-sm">
+                            <div>
+                              <h6 class="my-0">Plazo</h6>
+                              <small class="text-body-secondary">Meses</small>
+                            </div>
+                            <span class="text-body-secondary ">${
+                              info.plazo
+                            }</span>
+                          </li>
+                          <li class="list-group-item d-flex justify-content-between lh-sm">
+                            <div>
+                              <h6 class="my-0">Tasa Nominal Anual</h6>
+                            </div>
+                            <span class="text-body-secondary ">${
+                              info.tna
+                            } %</span>
+                          </li>
+                          <li class="list-group-item d-flex justify-content-between lh-sm">
+                            <div>
+                              <h6 class="my-0">Tasa Efectiva Anual</h6>
+                            </div>
+                            <span class="text-body-secondary ">${info.tea.toFixed(
+                              2
+                            )} %</span>
+                          </li>
+                          <li class="list-group-item d-flex justify-content-between lh-sm">
+                            <div>
+                              <h6 class="my-0">Tasa Efectiva Mensual</h6>
+                            </div>
+                            <span class="text-body-secondary ">$${info.tem.toFixed(
+                              2
+                            )} %</span>
+                          </li>
+                        </ul> `;
+  }
+  cardInfo.innerHTML = salida;
+}
+
+//Card Credito Seleccionado
 
 function solicitar() {
   Swal.fire({
@@ -9,23 +65,11 @@ function solicitar() {
     footer: '<a href="../index.html">Volver a Simular</a>',
   });
 }
-
-
-function credSelect() {
-  const info = cargarInfoLS();
-  info.length = 0 ? console.log("No hay info") : console.log(info);
-}
-
-
-//Borrar Todo
+//Borrar
 function borrarSolicitar() {
   localStorage.removeItem("pagos");
   localStorage.removeItem("info");
-  
-  document.getElementById("credSelect").innerHTML = "";
-  
+  document.getElementById("credSelect").innerHTML = `<p class="text-danger">Crédito sin simular</p>`;
 }
-
-
 
 credSelect();
